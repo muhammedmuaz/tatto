46ab952bb95656b515d0005a1a0440f560d7f6ef
kiranchaudhary18 <kiran.chaudhary.cg@gmail.com>
2025-02-17 14:20:35 +0530

    added six new routes in our categories

---
+++ vite_app/src/components/Ourcategories/RealisticCategory.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RealisticCategory = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default RealisticCategory
---
+++ vite_app/src/components/Ourcategories/StipplingCategory.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const StipplingCategory = () => {
+  return (
+    <div>
+      hi 
+    </div>
+  )
+}
+
+export default StipplingCategory
