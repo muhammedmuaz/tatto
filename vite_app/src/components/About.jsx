98d9d00cf0cc84b2ad5b320b933f507143fe0492
kiranchaudhary18 <kiran.chaudhary.cg@gmail.com>
2025-02-25 13:26:49 +0530

    added scroll animation in about page

--- vite_app/src/components/About.jsx
+++ vite_app/src/components/About.jsx
@@ -48,18 +48,59 @@
 
 // export default About
 
-
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import './About.css';
 
 const About = () => {
+  const workspaceRef = useRef(null);
+  const aboutContentRef = useRef(null);
+  const containerRef = useRef(null);
+  const visionMissionRef = useRef(null);
+  const team1Ref = useRef(null);
+  const team2Ref = useRef(null);
+  const team3Ref = useRef(null);
+  const team4Ref = useRef(null);
+
+  useEffect(() => {
+
+    if (workspaceRef.current) {
+      workspaceRef.current.classList.add('fade-in');
+    }
+    const handleScroll = () => {
+      const sections = [
+        // { ref: workspaceRef, className: 'enter-left' },
+        { ref: aboutContentRef, className: 'enter-right' },
+        { ref: containerRef, className: 'enter-left' },
+        { ref: visionMissionRef, className: 'enter-right' },
+        { ref: team1Ref, className: 'enter-left' },
+        { ref: team2Ref, className: 'enter-right' },
+        { ref: team3Ref, className: 'enter-left' },
+        { ref: team4Ref, className: 'enter-right' },
+      ];
+
+      sections.forEach(section => {
+        const rect = section.ref.current.getBoundingClientRect();
+        if (rect.top < window.innerHeight && rect.bottom >= 0) {
+          section.ref.current.classList.add(section.className);
+        }
+      });
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <div className="about-container">
       {/* Workspace Section */}
-      <div className="workspace">
+      <div className="workspace" >
         <h2 className="text">OUR WORKSPACE</h2>
-        <img
+        <img  ref={workspaceRef}
           className="studio"
           src="https://res.cloudinary.com/dnbayngfx/image/upload/v1738603822/studio-image-1_vnwgso.png"
           alt="Tattoo Studio"
@@ -67,8 +108,7 @@ const About = () => {
       </div>
 
       {/* About Section */}
-     
-      <div className="about-content">
+      <div className="about-content" ref={aboutContentRef}>
         <h2 className="text2">ABOUT OUR STUDIO</h2>
         <p className="text3">
           Welcome to Tattoo Dreamers Studio, your creative tattoo hub in Ahmedabad,
@@ -86,7 +126,7 @@ const About = () => {
       </div>
 
       {/* Before & After Image Section */}
-      <div className="container">
+      <div className="container" ref={containerRef}>
         <div className="image-wrapper">
           <img
             src="https://res.cloudinary.com/dnbayngfx/image/upload/v1738608090/before-pic-on-client_pfnocx.png"
@@ -99,49 +139,66 @@ const About = () => {
           alt="After Tattoo"
         />
       </div>
-      
 
-      {/* Vision Divider */}
-      <div className="vision-mission">
+      {/* Vision & Mission Section */}
+      <div className="vision-mission" ref={visionMissionRef}>
         <h2 className="vision">OUR VISION</h2>
-        <p className="vision-1">We dream of a world where tattoos are more than just
-           designs. They're like personal badges that speak to your soul, showing off your
-            uniqueness. At Alpha Tattoo Studio, we want to make getting a tattoo a meaningful and empower experience.</p>
-
-         <h2 className="mission">OUR MISSION</h2>
-         <p className="mission-1">To make sure you feel safe, comfortable, and excited about your tattoo journey. We're 
-          here to create an art that tells your story. We want you to leave our studio not just with a tattoo, but with a
-           piece of art that truly represents you.</p>   
-
-           <h2 className="team">OUR TEAM MEMBERS</h2>
+        <p className="vision-1">
+          We dream of a world where tattoos are more than just designs. They're like personal badges that speak to your soul, showing off your uniqueness. At Alpha Tattoo Studio, we want to make getting a tattoo a meaningful and empower experience.
+        </p>
+        <h2 className="mission">OUR MISSION</h2>
+        <p className="mission-1">
+          To make sure you feel safe, comfortable, and excited about your tattoo journey. We're here to create an art that tells your story. We want you to leave our studio not just with a tattoo, but with a piece of art that truly represents you.
+        </p>
+        <h2 className="team">OUR TEAM MEMBERS</h2>
       </div>
 
-      <div className="team1">
-        <img className="parth" src="https://res.cloudinary.com/dnbayngfx/image/upload/v1738815718/th_id_OIP_12_nlojdd.png"/>
+      {/* Team Members */}
+      <div className="team1" ref={team1Ref}>
+        <img
+          className="parth"
+          src="https://res.cloudinary.com/dnbayngfx/image/upload/v1738815718/th_id_OIP_12_nlojdd.png"
+          alt="Eric D'suza"
+        />
         <h2 className="eric">Eric D`suza</h2>
         <h2 className="parth2">ARTIST</h2>
       </div>
-      <div className="team2">
-      <img className="parth" src="https://res.cloudinary.com/dnbayngfx/image/upload/v1738780221/par-1_1_n107sg.png"/>
+      <div className="team2" ref={team2Ref}>
+        <img
+          className="parth"
+          src="https://res.cloudinary.com/dnbayngfx/image/upload/v1738780221/par-1_1_n107sg.png"
+          alt="Parth Vasani"
+        />
         <h2 className="parth1">PARTH VASANI</h2>
         <h2 className="parth2">ARTIST</h2>
       </div>
-
-      <div className="team3">
-      <img className="parth" src="https://res.cloudinary.com/dnbayngfx/image/upload/v1738815384/IMG_3884_1_n7nhl2.png"/>
+      <div className="team3" ref={team3Ref}>
+        <img
+          className="parth"
+          src="https://res.cloudinary.com/dnbayngfx/image/upload/v1738815384/IMG_3884_1_n7nhl2.png"
+          alt="Poufa"
+        />
         <h2 className="poufa">POUFA</h2>
         <h2 className="parth2">ARTIST</h2>
       </div>
-
-      <div className="team4">
-      <img className="parth" src="https://res.cloudinary.com/dnbayngfx/image/upload/v1738820859/th_id_OIP_4_lw9bgz.png"/>
+      <div className="team4" ref={team4Ref}>
+        <img
+          className="parth"
+          src="https://res.cloudinary.com/dnbayngfx/image/upload/v1738820859/th_id_OIP_4_lw9bgz.png"
+          alt="Sammy"
+        />
         <h2 className="sammy">SAMMY</h2>
         <h2 className="parth2">MANAGER</h2>
       </div>
 
+      {/* Button to Reach Studio */}
       <div className="about-button">
-        <Link className="button-1" to="/contact">REACH OUR STUDIO</Link>
+        <Link className="button-1" to="/contact">
+          REACH OUR STUDIO
+        </Link>
       </div>
+
+      
 
       <div className='about-footer'>
       <div>
