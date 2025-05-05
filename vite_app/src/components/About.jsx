cd62c112540864bd4b73a475283f9a293d012fa9
Muhammad Maaz <maazifran03360183798@gmail.com>
2025-05-05 17:02:24 +0500

    dw

--- vite_app/src/components/About.jsx
+++ vite_app/src/components/About.jsx
@@ -30,7 +30,7 @@
 //         to make it a memorable experience. So why wait? Come on in and let`s 
 //         create something amazing together. We are excited to meet you!
 //       </p>
-       
+
 //        <div className='container'>
 //         <div className='image-wrapper'>
 //       <img  src="https://res.cloudinary.com/dnbayngfx/image/upload/v1738608090/before-pic-on-client_pfnocx.png"/>
@@ -40,7 +40,7 @@
 //       </div>
 
 //       <div className="vision">
-        
+
 //       </div>
 //     </div>
 //   )
@@ -101,7 +101,7 @@ const About = () => {
       {/* Workspace Section */}
       <div className="workspace" >
         <h2 className="text">OUR WORKSPACE</h2>
-        <img  ref={workspaceRef}
+        <img ref={workspaceRef}
           className="studio"
           src="https://res.cloudinary.com/dnbayngfx/image/upload/v1738603822/studio-image-1_vnwgso.png"
           alt="Tattoo Studio"
@@ -152,20 +152,20 @@ const About = () => {
           To make sure you feel safe, comfortable, and excited about your tattoo journey. We're here to create an art that tells your story. We want you to leave our studio not just with a tattoo, but with a piece of art that truly represents you.
         </p>
         <div className="team-section">
-      {/* Background Video */}
-      <video autoPlay loop muted className="team-video">
-        <source
-          src="https://alphatattooindia.com/wp-content/uploads/2024/05/import_61c082d6899921.13979377.webm"
-          type="video/webm"
-        />
-        Your browser does not support the video tag.
-      </video>
+          {/* Background Video */}
+          <video autoPlay loop muted className="team-video">
+            <source
+              src="https://alphatattooindia.com/wp-content/uploads/2024/05/import_61c082d6899921.13979377.webm"
+              type="video/webm"
+            />
+            Your browser does not support the video tag.
+          </video>
 
-      {/* Overlay Content */}
-      <div className="team-overlay">
-        <h2 className="team">OUR TEAM MEMBERS</h2>
-      </div>
-    </div>
+          {/* Overlay Content */}
+          <div className="team-overlay">
+            <h2 className="team">OUR TEAM MEMBERS</h2>
+          </div>
+        </div>
       </div>
 
       {/* Team Members */}
@@ -213,9 +213,9 @@ const About = () => {
         </Link>
       </div>
 
-      
 
-       <div className='about-footer'>
+
+      <div className='about-footer'>
         <div>
           <img className='dreamers' src="https://res.cloudinary.com/dnbayngfx/image/upload/v1738673259/dreamers_ryrags.png" alt="Dreamers" />
         </div>
@@ -226,7 +226,7 @@ const About = () => {
             <li className="quick">QUICK LINKS</li>
             <li className="follow">FOLLOW US</li>
           </ul>
-          
+
           <div className="footer-3"></div>
           <div className="footer-4"></div>
           <div className="footer-5"></div>
@@ -265,22 +265,28 @@ const About = () => {
         </ul>
 
         <div className="circle-3">
-                 <Link to="https://github.com/kiranchaudhary18"  target="_blank">
-                 <img className="icon-3" src="https://res.cloudinary.com/dnbayngfx/image/upload/v1740680431/th_id_OIP_21_qktu4l.png" alt="Social Icon" />
-                 </Link>
-               </div>
-       
-               <div className="circle-4">
-               <Link to="https://x.com/home?lang=en-in"  target="_blank">
-                 <img className="icon-2" src="https://res.cloudinary.com/dnbayngfx/image/upload/v1738738668/th_id_OIP_10_dsd5bt.png" alt="Social Icon" />
-                 </Link>
-               </div>
-       
-               <div className="circle-5">
-               <Link to="https://www.instagram.com/chaudhary_kiran_022/?next=%2F"  target="_blank">
-                 <img className="icon-2" src="https://res.cloudinary.com/dnbayngfx/image/upload/v1738738751/th_id_OIP_11_rzyiqj.png" alt="Social Icon" />
-                 </Link>
-               </div>
+          <Link to="https://github.com/kiranchaudhary18" target="_blank">
+            <img className="icon-3" src="https://res.cloudinary.com/dnbayngfx/image/upload/v1740680431/th_id_OIP_21_qktu4l.png" alt="Social Icon" />
+          </Link>
+        </div>
+
+        <div className="circle-4">
+          <Link to="https://x.com/home?lang=en-in" target="_blank">
+            <img className="icon-2" src="https://res.cloudinary.com/dnbayngfx/image/upload/v1738738668/th_id_OIP_10_dsd5bt.png" alt="Social Icon" />
+          </Link>
+        </div>
+
+        <div className="circle-5">
+          <Link to="https://www.instagram.com/chaudhary_kiran_022/?next=%2F" target="_blank">
+            <img className="icon-2" src="https://res.cloudinary.com/dnbayngfx/image/upload/v1738738751/th_id_OIP_11_rzyiqj.png" alt="Social Icon" />
+          </Link>
+        </div>
+        {/* LinkedIn Logo */}
+        <div className="circle-linkedin">
+          <Link to="https://www.linkedin.com/" target="_blank">
+            <img className="icon-linkedin" src="https://res.cloudinary.com/dnbayngfx/image/upload/v1715000000/linkedin-icon.png" alt="LinkedIn Icon" />
+          </Link>
+        </div>
         <div className="home-row"></div>
       </div>
     </div>
